f0d792200f07f1b25f0e3b63974ffeefed8aac46
Xak-demon <Xakdem4@gmail.com>
2019-10-16 16:27:08 +0300

    3 commit today

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
 <?php
-echo 'Hello my Git!!!';
+echo "Hello my Git!!!<br/>";
+/*Time old*/
+$raw = '22. 11. 1968';
+$start = DateTime::createFromFormat('d. m. Y', $raw);
+echo "Start date: " . $start->format('m/d/Y') . "<br/>";
+/*Time*/
+$t=time();
+echo "Today is date: ". (date("Y-m-d",$t));
 ?>
 
 <!doctype html>
@@ -10,9 +17,11 @@ echo 'Hello my Git!!!';
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="css/main.css">
+    <link rel="shortcut icon" href="images/6.ico" type="images/x-icon">
     <title>Document</title>
 </head>
 <body>
 <h1>Hi Git!</h1>
+    <div class="img_logo"></div>
 </body>
 </html>
